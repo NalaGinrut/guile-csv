ded772922a43ccabf0b12b761fd4cbde933ec481
Nala Ginrut <nalaginrut@gmail.com>
2013-02-06 23:25:31 +0800

    Released v0.0.1

--- pkg-list.scm
+++ pkg-list.scm
@@ -1,5 +1,6 @@
-;;  Copyright (C) 2013
-;;      "Mu Lei" known as "NalaGinrut" <NalaGinrut@gmail.com>
+;;  Copyright (C) 2013 
+;;  Nala Ginrut <nalaginrut@gmail.com>
+
 ;;  This file is free software: you can redistribute it and/or modify
 ;;  it under the terms of the GNU General Public License as published by
 ;;  the Free Software Foundation, either version 3 of the License, or
@@ -13,23 +14,11 @@
 ;;  You should have received a copy of the GNU General Public License
 ;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-module (language csv spec)
-  #:use-module (system base language)
-  #:use-module (language csv compile-tree-il)
-  #:use-module (language csv parser)
-  #:export (csv))
-
-;;;
-;;; Language definition
-;;;
-
-;; Actually, CSV is not a language anyway, but we define this seudo language to 
-;; convert CSV format into s-expr.
-(define-language csv
-  #:title	"CSV language"
-  #:reader      (lambda (port env) 
-		  (csv-read port))
-  #:compilers   `((tree-il . ,compile-tree-il))
-  #:printer	write
-  )
-
+(package (csv (0 0 1))
+  (depends (csv))
+  (synopsis "Guile csv reader")
+  (description
+   "Guile csv reader")
+  (homepage "https://github.com/NalaGinrut/guile-csv/")
+  (libraries "xmlat.scm")
+  (documentation "README" "COPYING"))
