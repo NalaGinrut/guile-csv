516451cde6cf4b387567da8d52decac07ed20f0f
Nala Ginrut <nalaginrut@gmail.com>
2013-02-08 20:32:02 +0800

    update manual and version
    * README: update manual
    * pkg-list.scm: updated version

--- pkg-list.scm
+++ pkg-list.scm
@@ -14,7 +14,7 @@
 ;;  You should have received a copy of the GNU General Public License
 ;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(package (csv (0 0 1))
+(package (csv (0 0 2))
   (depends (csv))
   (synopsis "Guile csv reader")
   (description
