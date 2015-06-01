929cedf9ff6e5d0f2a9aeb4b9baebc59b19f5e77
Nala Ginrut <nalaginrut@gmail.com>
2015-06-01 12:04:37 +0800

    use autogen.sh to add missing

--- autogen.sh
+++ autogen.sh
@@ -1,31 +1,7 @@
 #!/bin/sh
-# Usage: sh -x ./autogen.sh
 
-######################################################################
-### announce build tool versions
-echo ""
-autoconf --version
-echo ""
-automake --version
-echo ""
-
-# Typical MacOS X installations rename 'libtoolize' to 'glibtoolize', so
-# adjust to that.
-if test "`uname -s`" = "Darwin"; then
-  glibtoolize --version
-else
-  libtoolize --version
-fi
-
-echo ""
-${M4:-m4} --version
-echo ""
-flex --version
-echo ""
-
-######################################################################
-### update infrastructure
-
-autoreconf -i --force --verbose
-
-echo "Now run configure and make."
+libtoolize --force
+aclocal
+autoheader
+automake --force-missing --add-missing
+autoconf
